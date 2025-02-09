c2951d58a1dc29f0f1000880ecc54deefca82f82
Jesse Freeman <jessefreeman@gmail.com>
2025-02-09 18:25:15 +0000

    Feb 9, 2025, 6:25 PM

--- .eleventy/gulpfile.js
+++ .eleventy/gulpfile.js
@@ -3,8 +3,6 @@ const postcss   = require("gulp-postcss");
 const precss    = require('precss');
 const cssnano   = require('cssnano');
 
-
-
 /*
   generate the css with post css
 */
@@ -14,15 +12,12 @@ gulp.task('css', function () {
     .pipe(gulp.dest('site/_includes/css'));
 });
 
-
-
 /*
   Watch folders for changess
 */
 gulp.task("watch", function() {
   gulp.watch('css/**/*.css', gulp.parallel('css'));
 });
-
 
 /*
   Let's build this sucker.
