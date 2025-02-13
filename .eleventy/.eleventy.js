a028bb4d548b9ed7dbb3a365d007f3ee795e3494
Jesse Freeman <jessefreeman@gmail.com>
2025-02-13 10:57:25 +0000

    Fixed data on index page.

--- .eleventy/.eleventy.js
+++ .eleventy/.eleventy.js
@@ -93,6 +93,14 @@ module.exports = function (eleventyConfig) {
     })
   );
 
+  eleventyConfig.addFilter("shortDate", (date) => {
+    const d = new Date(date);
+    const day = ("0" + d.getDate()).slice(-2);
+    const month = ("0" + (d.getMonth() + 1)).slice(-2);
+    const year = d.getFullYear();
+    return `${day}-${month}-${year}`;
+  });
+
   eleventyConfig.addFilter("map", (array, property) =>
     array.map((item) => item[property])
   );
